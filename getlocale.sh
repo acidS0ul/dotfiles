e9ff55110838fdafb5fed3c646925d07e10919b5
Denis Semenov <37097033+acidS0ul@users.noreply.github.com>
2024-12-10 21:40:45 +0300

    Update getlocale.sh

--- getlocale.sh
+++ getlocale.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-res=$(xset -q | sed -rn 's/.*LED mask.*(.)[[:xdigit:]]{3}$/\1/p' | awk '{if ($1 == 0) print "en"; else print "ru";}')
-echo "$res"
+
+mask=0
+locale=("en" "ru" "EN" "RU")
+
+group2=$(xset q | grep -E "Group 2:\s+\w{2,3}" | awk '{print $4}')
+capslc=$(xset q | grep -E "Caps .*:\s+\w{2,3}" | awk '{print $4}')
+
+if [[ "$group2" == "on" ]]; then
+    mask=$((mask + 1))
+fi
+
+if [[ "$capslc" == "on" ]]; then
+    mask=$((mask + 2))
+fi
+
+
+echo ${locale[mask]}
